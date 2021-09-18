7728ec97cae2684e72e998b2c3fa6019f7a5db16
Maximilian Keßler <max@maximilian-kessler.de>
2021-09-18 14:38:20 +0200

    take environment directly in edit method

--- scripts/edit.py
+++ scripts/edit.py
@@ -5,14 +5,11 @@ from pathlib import Path
 import os
 
 
-def edit(filepath: Path, rootpath: Path = None, texinputs=None):
-    env = dict(os.environ)
-    if texinputs:
-        env["TEXINPUTS"] = texinputs
+def edit(filepath: Path, rootpath: Path = None, env=os.environ):
     if not rootpath:
         rootpath = filepath
     subprocess.Popen([
         "termite",
         "-e",
         f"vim --servername tex-vorlesung --remote-silent {str(filepath)}"
-    ], env=env, cwd=str(rootpath.root))
+    ], env=env, cwd=str(rootpath))
