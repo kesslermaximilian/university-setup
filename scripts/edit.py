d028139860ae06c402b14f95c204f3fe608db4ec
Maximilian Keßler <max@maximilian-kessler.de>
2021-09-18 14:16:53 +0200

    provide optional arguments to set the path root and thetexinputs variable in edit command

--- scripts/edit.py
+++ scripts/edit.py
@@ -2,11 +2,17 @@
 
 import subprocess
 from pathlib import Path
+import os
 
 
-def edit(filepath: Path):
+def edit(filepath: Path, rootpath: Path = None, texinputs=None):
+    env = dict(os.environ)
+    if texinputs:
+        env["TEXINPUTS"] = texinputs
+    if not rootpath:
+        rootpath = filepath
     subprocess.Popen([
         "termite",
         "-e",
         f"vim --servername tex-vorlesung --remote-silent {str(filepath)}"
-    ])
+    ], env=env, cwd=str(rootpath.root))
